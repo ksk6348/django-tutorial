14a091b8eb69da9e27e9e1ba88ca4a5111a446ea
ohya <ksk6348@gmail.com>
2019-02-12 21:48:17 +0900

    first commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "text")
+    fields = ("title", "text")
+
+
 admin.site.register(Post)
